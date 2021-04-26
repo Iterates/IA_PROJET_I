40506e1b650ad1367feef4d8d5041372c8aada74
Iterates <trent.nergui@gmail.com>
2021-04-26 08:59:29 -0400

    Ajout des fichiers utilitaires

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+# Importation des modules
+import sys
+from db_credential import PostgreSQLCredential
+from klustr_dao import PostgreSQLKlustRDAO
+from klustr_widget import KlustRDataSourceViewWidget
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QApplication
+from __feature__ import snake_case, true_property
+
+
 if __name__ == "__main__":
-    print("Hello World!")
+    app = QApplication(sys.argv)
+
+    credential = PostgreSQLCredential(host='jcd-prof-cvm-69b5.aivencloud.com', port=11702, database='data_kit', user='klustr_reader', password='h$2%1?')
+    klustr_dao = PostgreSQLKlustRDAO(credential)
+    source_data_widget = KlustRDataSourceViewWidget(klustr_dao)
+    source_data_widget.show()
+
+    sys.exit(app.exec_())
