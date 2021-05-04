901a9fb4a76ee3997c5484b8e14e8458ceac1fd1
Iterates <trent.nergui@gmail.com>
2021-05-04 18:16:02 -0400

    Travail sur le perimetre

--- klustr_engine.py
+++ klustr_engine.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 # from klustr_feed import *
 from klustr_utils import *
 from db_credential import PostgreSQLCredential
@@ -45,18 +46,19 @@ class KlustEngine():
             centroid = self.centroid_function(i)
             radius = self.centroid_radius(i, centroid)
             aire = self.area_function(i)
-            perimetre = np.count_nonzero(self.perimeter_image(i) * 1)
+#           perimetre = np.count_nonzero(self.perimeter_image(i) * 1)
+            perimetre = self.calcul_perimetre(i)
             cercle = self.draw_circle_around_form(i, centroid, radius)
             aire_cerlce = self.area_function(cercle)
-            
+
             x.append(self.knn_axe1(aire, perimetre))
             y.append(self.knn_axe2(aire, aire_cerlce))
             z.append(self.knn_axe3(i, centroid))
             # print(self.dist_moyenne_centre(i))
             # print(self.dist_moy_centre(self.perimeter_image(i) * 1, centroid))
         print(x, y, z)
+        print(np.count_nonzero(np.array(x) > 1))
         return x, y, z
-
 
     def area_function(self, array):
         return np.count_nonzero(array)
@@ -69,15 +71,12 @@ class KlustEngine():
         ele = np.count_nonzero(array == 1)
         return y/ele, x/ele
     
-
     #dessine un cerle autour de l'image
     def draw_circle_around_form(self, image, centroid, radius):
         col_mesh, row_mesh = np.meshgrid(np.arange(image.shape[1]), np.arange(image.shape[0]))
         return np.where(np.sqrt((row_mesh - centroid[0])**2 + (col_mesh - centroid[1])**2) <= radius, np.ones(image.shape), np.zeros(image.shape))
 
     # def calculate_area_circle_around_form(self):
-
-
 
     # def ext_form_area(self):
     #     return np.count_nonzero(self.circle_around_form) - self.form_area
@@ -102,6 +101,16 @@ class KlustEngine():
         dist = np.sqrt(pow((row_mesh - centroid[0]), 2) + pow((col_mesh - centroid[1]), 2))
         a = dist[image_perimetre == 1]
         return (np.min(a), np.mean(a), np.max(a))
+
+    def calcul_perimetre(self, image):
+        perimetre = self.perimeter_image(image)
+        m_top = perimetre[0:-2, 1:-1]
+        m_right = perimetre[1:-1, 2:]
+        m_left = perimetre[1:-1, 0:-2]
+        m_bottom = perimetre[2:, 1:-1]
+        m_total = m_top + m_right + m_left + m_bottom
+        m_total = np.pad(m_total, pad_width=1, mode="constant", constant_values=0)
+        return np.sum(perimetre * np.where(m_total, 1, np.sqrt(2)))
 
     # calcul du périmètre
     def perimeter_image(self, image):
@@ -129,7 +138,6 @@ class KlustEngine():
         distance = self.dist_moy_centre(self.perimeter_image(image) * 1, centroid)# / self.centroid_radius(image, centroid)
         return distance[1] / distance[2]
 
-
 if __name__ == '__main__':
     # array = np.array([220,220], dtype = np.uint8)
     credential = PostgreSQLCredential(host='jcd-prof-cvm-69b5.aivencloud.com', port=11702, database='data_kit', user='klustr_reader', password='h$2%1?')
@@ -137,90 +145,7 @@ if __name__ == '__main__':
     label_images = np.array(klustr_dao.image_from_dataset('ABC', True), dtype=object)[:,1]
     raw_images = np.array(klustr_dao.image_from_dataset('ABC', True), dtype=object)[:, 6]
     ke = KlustEngine(raw_images, label_images)
+    print(label_images)
     # print(klustr_dao.image_from_dataset('ABC', True))
 
-    def perimeter_function(image):
-        m_left = image[1:-1, 0:-2]
-        m_center = image[1:-1, 1:-1]
-        m_top = image[0:-2, 1:-1]
-        m_right = image[1:-1, 2:]
-        m_bottom = image[2:, 1:-1]
-        temp_bottom = np.logical_and(m_center, np.logical_xor(m_bottom, m_center, dtype=np.uint8))
-        temp_right = np.logical_and(m_center, np.logical_xor(m_right, m_center, dtype=np.uint8))
-        temp_top = np.logical_and(m_center, np.logical_xor(m_top, m_center, dtype=np.uint8))
-        temp_left = np.logical_and(m_center, np.logical_xor(m_left, m_center, dtype=np.uint8))
-        return np.logical_or(temp_right, np.logical_or(temp_bottom, np.logical_or(temp_top, temp_left, dtype=np.uint8)))
     
-    def extraire_coord(image):
-        m_coord_x, m_coord_y = np.meshgrid(np.arange(image.shape[1]), np.arange(image.shape[0])) 
-        m_coord_x = m_coord_x[image == 1] + 1
-        m_coord_y = m_coord_y[image == 1] + 1
-        coord = np.empty((m_coord_x.shape[0], 2), dtype=np.uint16)    
-        coord[:, 0] = m_coord_x
-        coord[:, 1] = m_coord_y
-        return coord
-
-    def create_image(size):
-        return np.zeros(size, dtype=np.uint8)
-
-    def draw_circle_the_numpy_way(image, center, radius): # the matrix way!
-        col_mesh, row_mesh = np.meshgrid(np.arange(image.shape[1]), np.arange(image.shape[0]))
-        image[:] = np.logical_or(image[:], (np.sqrt((row_mesh - center[0])**2 + (col_mesh - center[1])**2) <= radius).astype(np.uint8))
-
-    def dist_moyenne_centre(image):
-        m_coord_x, m_coord_y = np.meshgrid(np.arange(image.shape[1]), np.arange(image.shape[0])) 
-        centroid_x, centroid_y = centroid_function(image)[1], centroid_function(image)[0]
-        return np.mean(np.sqrt((m_coord_x - centroid_x)**2 + (m_coord_y - centroid_y)**2))
-
-    def centroid_function(array):
-        coordx, coordy = np.meshgrid(np.arange(len(array[0,:])), np.arange(len(array[:,0])))
-        x = np.sum(coordx * array)
-        y = np.sum(coordy * array)
-        ele = np.count_nonzero(array == 1)
-        return y/ele, x/ele
-
-    img = create_image((15, 15))
-
-    draw_circle_the_numpy_way(img, (6, 6), 4)
-
-    print(img)
-
-    new_img = perimeter_function(img)
-    x = extraire_coord(new_img)
-    print(new_img * 1)
-    print(np.sum(img))
-    print(np.sum(new_img))
-    x = dist_moyenne_centre(new_img)
-    array_of_arrays = np.array([np.arange(16).reshape(4,4), np.arange(16).reshape(4,4), np.arange(16).reshape(4,4)])
-    s = lambda arr : np.sum(arr)
-    vect_s = np.vectorize(s, signature="(m,n)->()")
-    sum_array = vect_s(array_of_arrays)
-    print(array_of_arrays)
-    print(sum_array)
-
-    def d_print(function):
-        def wrapper(*args, **kwargs):
-            function(*args, **kwargs)
-            function(*args, **kwargs)
-        return wrapper
-
-
-    def f_chrono(function):
-        def wrapper(*args, **kwargs):
-            start = time.time_ns()
-            function(*args, **kwargs)
-            end = time.time_ns()
-            return print(end - start)
-        return wrapper
-
-    @f_chrono 
-    @d_print
-    def fonction(hh):
-        print(hh)
-
-    @f_chrono
-    def sum_of_array(array):
-        return np.sum(array)
-
-    fonction("pp")
-    sum_of_array(new_img)
